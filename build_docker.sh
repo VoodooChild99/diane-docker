72493e6b8a712221f42f7250f77a26bb45da7795
Rayhub <1102290321@qq.com>
2024-01-06 15:39:01 +0000

    remove containers before building the image

--- build_docker.sh
+++ build_docker.sh
@@ -2,7 +2,15 @@
 
 # set PROXY_ADDRESS if you need a proxy
 
-docker build -t diane . \
+# remove related containers
+IMAGE=diane
+DOCKER_NAME=$(docker ps --filter ancestor=$IMAGE:latest -a --format "{{.Names}}")
+
+if [[ -n $DOCKER_NAME ]]; then
+    docker rm $DOCKER_NAME
+fi
+
+docker build -t $IMAGE . \
     --build-arg "HTTP_PROXY=$PROXY_ADDRESS" \
     --build-arg "HTTPS_PROXY=$PROXY_ADDRESS" \
     --build-arg "http_proxy=$PROXY_ADDRESS" \
