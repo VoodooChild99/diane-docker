23657e5fcd5773db1c7b54d6893cb904cabdc185
Rayhub <1102290321@qq.com>
2024-01-11 13:36:14 +0800

    stop containers before removing them

--- build_docker.sh
+++ build_docker.sh
@@ -7,6 +7,7 @@ IMAGE=diane
 DOCKER_NAME=$(docker ps --filter ancestor=$IMAGE:latest -a --format "{{.Names}}")
 
 if [[ -n $DOCKER_NAME ]]; then
+    docker stop $DOCKER_NAME
     docker rm $DOCKER_NAME
 fi
 
