ad8cd313b374d82d66ee87712348a69cd28e9c91
Rayhub <1102290321@qq.com>
2024-01-12 14:03:42 +0800

    bypass frida detection (e.g., JD)

--- container-scripts/prep_frida.py
+++ container-scripts/prep_frida.py
@@ -9,6 +9,10 @@ if __name__ == '__main__':
 
     adbd.adb_su_cmd('kill -9 `pgrep -f frida-server`')
     adbd.adb_su_cmd('rm /data/local/tmp/frida-server-11.0.2-android-arm64')
+    adbd.adb_su_cmd('rm /data/local/tmp/frd1102')
+    adbd.adb_cmd(['forward', '--remove-all'])
+    adbd.adb_cmd(['forward', 'tcp:27047', 'tcp:9999'])
     adbd.adb_cmd(['push', '/root/frida-server-11.0.2-android-arm64', '/data/local/tmp'])
     adbd.adb_su_cmd('chmod +x /data/local/tmp/frida-server-11.0.2-android-arm64')
-    adbd.adb_su_cmd('/data/local/tmp/frida-server-11.0.2-android-arm64')
+    adbd.adb_su_cmd('cp /data/local/tmp/frida-server-11.0.2-android-arm64 /data/local/tmp/frd1102')
+    adbd.adb_su_cmd('/data/local/tmp/frd1102 -l 0.0.0.0:9999')
